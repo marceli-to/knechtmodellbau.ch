bfbe13c2108ebd8a9dc428b6ca167f9cc2d87708
Marcel Stadelmann <m@marceli.to>
2024-10-28 11:12:21 +0100

    wip

--- resources/views/components/team/member.blade.php
+++ resources/views/components/team/member.blade.php
@@ -2,14 +2,15 @@
   'name', 
   'title' => null, 
   'email' => null, 
-  'image'
+  'image',
+  'overlap' => false
 ])
-<figure {{ $attributes->merge(['class' => 'md:col-span-6 lg:col-span-3']) }}>
+<figure {{ $attributes->merge(['class' => 'md:col-span-6 lg:col-span-3 relative']) }}>
   <img 
     src="{{ $image }}" 
     alt="Member"
     class="w-full h-auto">
-  <figcaption class="pt-16">
+  <figcaption class="pt-16 {{ $overlap ? 'lg:absolute lg:-bottom-94 lg:!pt-0 lg:left-0 w-full h-auto' : '' }}">
     <h2 class="font-semibold">
       {{ $name }}
     </h2>
--- resources/views/pages/portrait.blade.php
+++ resources/views/pages/portrait.blade.php
@@ -6,6 +6,7 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia facilis consequatur at in, est libero ad. Officia soluta ducimus eum est, eveniet quaerat sequi, asperiores, consequuntur facilis laudantium error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia facilis consequatur at in, est libero ad. Officia soluta ducimus eum est, eveniet quaerat sequi, asperiores, consequuntur facilis laudantium error.</p>
   </div>
 </div>
+
 <div class="md:grid md:grid-cols-12 md:gap-x-20 mt-40 md:mt-60">
   <div class="md:col-span-12">
     <div class="flex flex-col md:grid md:grid-cols-12 gap-y-40 md:gap-x-20 md:gap-y-60">
@@ -32,6 +33,90 @@
         title="Assistent" 
         email="john.johnson@example.com" 
         image="https://picsum.photos/800/1200" 
+      />
+    </div>
+  </div>
+</div>
+
+<div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0 mt-45 md:mt-70 lg:mb-154">
+  <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+    <div>
+      <img src="https://picsum.photos/1200/800" alt="Portrait" class="w-full h-auto aspect-[760/500]">
+    </div>
+    <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+      <x-team.member 
+        name="John Doe" 
+        title="Projektleiter" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200" 
+        class="md:!col-span-6"
+        overlap
+      />
+      <x-team.member 
+        name="Jane Master" 
+        title="Modellbauer" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200"
+        class="md:!col-span-6"
+        overlap
+      />
+    </div>
+  </div>
+  <div class="md:col-span-12 lg:col-span-6 bg-red-100 lg:relative">
+    <img src="https://picsum.photos/800/1200" alt="Portrait" class="lg:absolute w-full h-auto lg:h-full object-cover">
+  </div>
+</div>
+
+<div class="md:grid md:grid-cols-12 md:gap-x-20 mt-45 md:mt-70 lg:mb-154">
+  <div class="flex flex-col md:col-span-12 lg:grid lg:grid-cols-12 gap-y-20 lg:gap-x-20">
+    <div class="lg:col-span-6">
+      <img src="https://picsum.photos/1200/800" alt="Portrait" class="w-full h-auto lg:h-full aspect-[760/500]">
+    </div>
+    <div class="flex flex-col lg:col-span-6 md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+      <x-team.member 
+        name="John Doe" 
+        title="Projektleiter" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200" 
+        class="md:!col-span-6"
+        overlap
+      />
+      <x-team.member 
+        name="Jane Master" 
+        title="Modellbauer" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200"
+        class="md:!col-span-6"
+        overlap
+      />
+    </div>
+  </div>
+</div>
+
+<div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0 mt-45 md:mt-70 lg:mb-94">
+  <div class="md:col-span-12 lg:col-span-6 bg-red-100 lg:relative">
+    <img src="https://picsum.photos/800/1200" alt="Portrait" class="lg:absolute w-full h-auto lg:h-full object-cover">
+  </div>
+  <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+    <div>
+      <img src="https://picsum.photos/1200/800" alt="Portrait" class="w-full h-auto aspect-[760/500]">
+    </div>
+    <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+      <x-team.member 
+        name="John Doe" 
+        title="Projektleiter" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200" 
+        class="md:!col-span-6"
+        overlap
+      />
+      <x-team.member 
+        name="Jane Master" 
+        title="Modellbauer" 
+        email="john.doe@example.com" 
+        image="https://picsum.photos/800/1200"
+        class="md:!col-span-6"
+        overlap
       />
     </div>
   </div>
