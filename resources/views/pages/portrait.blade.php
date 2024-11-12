77b6d1a74d1b736d2ff6bd087b1bbd2556451005
Marcel Stadelmann <m@marceli.to>
2024-11-12 23:48:50 +0100

    wip

--- resources/views/pages/portrait.blade.php
+++ resources/views/pages/portrait.blade.php
@@ -7,145 +7,267 @@
   </div>
 </div>
 
-<div class="md:grid md:grid-cols-12 md:gap-x-20 mt-40 md:mt-60">
-  <div class="md:col-span-12">
-    <div class="flex flex-col md:grid md:grid-cols-12 gap-y-40 md:gap-x-20 md:gap-y-60">
-      <div class="md:col-span-6 lg:col-span-3">
-        <x-team.portrait 
-          name="Christoph Smolders" 
-          title="Geschäftsführer" 
-          email="ch.smolders@knechtmodellbau.ch"
-          bio="Seit 1988 bei Knecht Modellbau"
-          image="knecht_modellbau_portrait_01_christoph_smolders" />
-      </div>
-      <div class="md:col-span-6 lg:col-span-3">
-        <x-team.visual image="knecht_modellbau_portrait_02" />
-      </div>
-      <div class="md:col-span-6 lg:col-span-3">
-        <x-team.portrait 
-          name="Brogle Lee" 
-          title="Architekturmodellbauer" 
-          bio="Seit 2019 bei Knecht Modellbau" 
-          image="knecht_modellbau_portrait_03_brogle_lee" />
-      </div>
-      <div class="md:col-span-6 lg:col-span-3">
-        <x-team.portrait 
-          name="Simon Littwin" 
-          title="Lernender Architekturmodellbau" 
-          bio="Seit 2021 bei Knecht Modellbau" 
-          image="knecht_modellbau_portrait_04_simon_littwin" />
+<div class="flex flex-col space-y-40 md:space-y-60 mt-40 md:mt-60">
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="md:col-span-12">
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-40 md:gap-x-20 md:gap-y-60">
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Christoph Smolders" 
+            title="Geschäftsführer" 
+            email="ch.smolders@knechtmodellbau.ch"
+            bio="Seit 1988 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_01_christoph_smolders" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.visual image="knecht_modellbau_portrait_02" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Brogle Lee" 
+            title="Architekturmodellbauer" 
+            bio="Seit 2019 bei Knecht Modellbau" 
+            image="knecht_modellbau_portrait_03_brogle_lee" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Simon Littwin" 
+            title="Lernender Architekturmodellbau" 
+            bio="Seit 2021 bei Knecht Modellbau" 
+            image="knecht_modellbau_portrait_04_simon_littwin" />
+        </div>
       </div>
     </div>
   </div>
-</div>
-
-<div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0 mt-45 md:mt-70">
-  <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
-    <div>
-      <x-team.visual image="knecht_modellbau_portrait_05" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+  <div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0">
+    <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+      <div>
+        <x-team.visual image="knecht_modellbau_portrait_05" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+      </div>
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Roger Baumgartner" 
+            title="Architekturmodellbauer / Kunststofftechiker HF" 
+            email="baumgartner@knechtmodellbau.ch"
+            bio="Seit 1994 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_06_roger_baumgartner" 
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Dario Bugmann" 
+            title="Architekturmodellbauer / Hochbauzeichner" 
+            email="bugmann@knechtmodellbau.ch"
+            bio="Bachelor of Arts in Architektur" 
+            image="knecht_modellbau_portrait_07_dario_bugmann"
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+      </div>
     </div>
-    <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
-      <div class="md:col-span-6 lg:col-span-6">
-        <x-team.portrait 
+    <div class="md:col-span-12 lg:col-span-6 lg:relative">
+      <picture>
+        <source srcset="/img/knecht_modellbau_portrait_08-700w.webp 700w, /img/knecht_modellbau_portrait_08-1200w.webp 1200w, /img/knecht_modellbau_portrait_08-1600w.webp 1600w" type="image/webp">
+        <source srcset="/img/knecht_modellbau_portrait_08-700w.jpg 700w, /img/knecht_modellbau_portrait_08-1200w.jpg 1200w, /img/knecht_modellbau_portrait_08-1600w.jpg 1600w" type="image/jpeg">
+        <img src="/img/knecht_modellbau_portrait_08-700w.jpg" alt="" title="" height="939" width="700" class="w-full block lg:absolute lg:!h-full object-cover">
+      </picture>
+    </div>
+    <div class="hidden lg:col-span-12 lg:grid lg:grid-cols-12 lg:gap-x-20">
+      <div class="lg:col-span-3">
+        <x-team.caption 
           name="Roger Baumgartner" 
           title="Architekturmodellbauer / Kunststofftechiker HF" 
           email="baumgartner@knechtmodellbau.ch"
-          bio="Seit 1994 bei Knecht Modellbau"
-          image="knecht_modellbau_portrait_06_roger_baumgartner" 
-          class="md:!col-span-6"
-          captionClass="lg:hidden"
-        />
+          bio="Seit 1994 bei Knecht Modellbau" />
       </div>
-      <div class="md:col-span-6 lg:col-span-6">
-        <x-team.portrait 
+      <div class="lg:col-span-3">
+        <x-team.caption 
           name="Dario Bugmann" 
           title="Architekturmodellbauer / Hochbauzeichner" 
           email="bugmann@knechtmodellbau.ch"
-          bio="Bachelor of Arts in Architektur" 
-          image="knecht_modellbau_portrait_07_dario_bugmann"
-          class="md:!col-span-6"
-          captionClass="lg:hidden"
-        />
+          bio="Bachelor of Arts in Architektur" />
       </div>
     </div>
   </div>
-  <div class="md:col-span-12 lg:col-span-6 lg:relative">
-    <picture>
-      <source srcset="/img/knecht_modellbau_portrait_08-700w.webp 700w, /img/knecht_modellbau_portrait_08-1200w.webp 1200w, /img/knecht_modellbau_portrait_08-1600w.webp 1600w" type="image/webp">
-      <source srcset="/img/knecht_modellbau_portrait_08-700w.jpg 700w, /img/knecht_modellbau_portrait_08-1200w.jpg 1200w, /img/knecht_modellbau_portrait_08-1600w.jpg 1600w" type="image/jpeg">
-      <img src="/img/knecht_modellbau_portrait_08-700w.jpg" alt="" title="" height="939" width="700" class="w-full block lg:absolute lg:!h-full object-cover">
-    </picture>
-  </div>
-  <div class="hidden lg:col-span-12 lg:grid lg:grid-cols-12 lg:gap-x-20">
-    <div class="lg:col-span-3">
-      <x-team.caption 
-        name="Roger Baumgartner" 
-        title="Architekturmodellbauer / Kunststofftechiker HF" 
-        email="baumgartner@knechtmodellbau.ch"
-        bio="Seit 1994 bei Knecht Modellbau" />
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="md:col-span-12">
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-40 md:gap-x-20 md:gap-y-60">
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Benjamin Kämpf" 
+            title="Architekturmodellbauer" 
+            email="kaempf@knechtmodellbau.ch"
+            bio="Seit 2016 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_09_benjamin_kaempf" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.visual image="knecht_modellbau_portrait_10" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Alisha Smolders" 
+            title="Administration / Rechnungswesen" 
+            bio="Seit 2023 bei Knecht Modellbau" 
+            email="info@knechtmodellbau.ch"
+            image="knecht_modellbau_portrait_11_alisha_smolders" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-3">
+          <x-team.portrait 
+            name="Paul Conrad" 
+            title="Architekturmodellbauer" 
+            bio="Seit 2003 bei Knecht Modellbau" 
+            email="conrad@knechtmodellbau.ch"
+            image="knecht_modellbau_portrait_12_paul_conrad" />
+        </div>
+      </div>
     </div>
-    <div class="lg:col-span-3">
-      <x-team.caption 
-        name="Dario Bugmann" 
-        title="Architekturmodellbauer / Hochbauzeichner" 
-        email="bugmann@knechtmodellbau.ch"
-        bio="Bachelor of Arts in Architektur" />
+  </div>
+  <div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0">
+    <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+      <div>
+        <x-team.visual image="knecht_modellbau_portrait_13" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+      </div>
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Claudia Smolders" 
+            title="Administration / Rechnungswesen" 
+            email="c.smolders@knechtmodellbau.ch"
+            bio="Seit 2000 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_14_claudia_smolders" 
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.visual image="knecht_modellbau_portrait_15" />
+        </div>
+      </div>
+    </div>
+    <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+      <div>
+        <x-team.visual image="knecht_modellbau_portrait_13" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+      </div>
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Joya Stettler" 
+            title="Architekturmodellbauerin" 
+            bio="Seit 2023 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_16_joya_stettler" 
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Franziska Loser" 
+            title="Architekturmodellbauerin" 
+            bio="Seit 2018 bei Knecht Modellbau" 
+            image="knecht_modellbau_portrait_17_franziska_loser"
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:col-span-12 lg:grid lg:grid-cols-12 lg:gap-x-20">
+      <div class="lg:col-span-3">
+        <x-team.caption 
+          name="Claudia Smolders" 
+          title="Administration / Rechnungswesen" 
+          email="c.smolders@knechtmodellbau.ch"
+          bio="Seit 2000 bei Knecht Modellbau" />
+      </div>
+      <div class="lg:col-span-3 lg:col-start-7">
+        <x-team.caption 
+          name="Joya Stettler" 
+          title="Architekturmodellbauerin" 
+          bio="Seit 2023 bei Knecht Modellbau" />
+      </div>
+      <div class="lg:col-span-3">
+        <x-team.caption 
+          name="Franziska Loser" 
+          title="Architekturmodellbauerin" 
+          bio="Seit 2018 bei Knecht Modellbau" />
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0">
+    <div class="md:col-span-12 lg:col-span-6 lg:relative">
+      <picture>
+        <source srcset="/img/knecht_modellbau_portrait_18-700w.webp 700w, /img/knecht_modellbau_portrait_18-1200w.webp 1200w, /img/knecht_modellbau_portrait_18-1600w.webp 1600w" type="image/webp">
+        <source srcset="/img/knecht_modellbau_portrait_18-700w.jpg 700w, /img/knecht_modellbau_portrait_18-1200w.jpg 1200w, /img/knecht_modellbau_portrait_18-1600w.jpg 1600w" type="image/jpeg">
+        <img src="/img/knecht_modellbau_portrait_18-700w.jpg" alt="" title="" height="939" width="700" class="w-full block lg:absolute lg:!h-full object-cover">
+      </picture>
+    </div>
+    <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+      <div>
+        <x-team.visual image="knecht_modellbau_portrait_19" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+      </div>
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Ronja Lingenhag" 
+            title="Architekturmodellbauerin" 
+            bio="Seit 2020 bei Knecht Modellbau"
+            image="knecht_modellbau_portrait_20_ronja_lingenhag" 
+            class="md:!col-span-6"
+            captionClass="lg:hidden"
+          />
+        </div>
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.portrait 
+            name="Aita Schäfli" 
+            title="Lernende Architekturmodellbau" 
+            bio="Seit 2023 bei Knecht Modellbau"
+            class="md:!col-span-6"
+            image="knecht_modellbau_portrait_21_aita_schaefli"
+            captionClass="lg:hidden"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:col-span-12 lg:grid lg:grid-cols-12 lg:gap-x-20">
+      <div class="lg:col-span-3 lg:col-start-7">
+        <x-team.caption 
+          name="Ronja Lingenhag" 
+          title="Architekturmodellbauerin" 
+          bio="Seit 2020 bei Knecht Modellbau" />
+      </div>
+      <div class="lg:col-span-3 lg:col-start-10">
+        <x-team.caption 
+          name="Aita Schäfli" 
+          title="Lernende Architekturmodellbau" 
+          bio="Seit 2023 bei Knecht Modellbau" />
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0">
+    <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
+      <div>
+        <x-team.visual image="knecht_modellbau_portrait_22" class="w-full h-auto aspect-[760/460]" height="460" width="760" />
+      </div>
+      <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.visual image="knecht_modellbau_portrait_23" />
+        </div>
+        <div class="md:col-span-6 lg:col-span-6">
+          <x-team.visual image="knecht_modellbau_portrait_24" />
+        </div>
+      </div>
+    </div>
+    <div class="md:col-span-12 lg:col-span-6 lg:relative">
+      <picture>
+        <source srcset="/img/knecht_modellbau_portrait_25-700w.webp 700w, /img/knecht_modellbau_portrait_25-1200w.webp 1200w, /img/knecht_modellbau_portrait_25-1600w.webp 1600w" type="image/webp">
+        <source srcset="/img/knecht_modellbau_portrait_25-700w.jpg 700w, /img/knecht_modellbau_portrait_25-1200w.jpg 1200w, /img/knecht_modellbau_portrait_25-1600w.jpg 1600w" type="image/jpeg">
+        <img src="/img/knecht_modellbau_portrait_25-700w.jpg" alt="" title="" height="939" width="700" class="w-full block lg:absolute lg:!h-full object-cover">
+      </picture>
     </div>
   </div>
 </div>
-
-{{-- <div class="md:grid md:grid-cols-12 md:gap-x-20 mt-45 md:mt-70">
-  <div class="flex flex-col md:col-span-12 lg:grid lg:grid-cols-12 gap-y-20 lg:gap-x-20">
-    <div class="lg:col-span-6">
-      <img src="https://picsum.photos/1200/800" alt="Portrait" class="w-full h-auto lg:h-full aspect-[760/500]">
-    </div>
-    <div class="flex flex-col lg:col-span-6 md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
-      <x-team.portrait 
-        name="John Doe" 
-        title="Projektleiter" 
-        email="john.doe@example.com" 
-        image="https://picsum.photos/800/1200" 
-        class="md:!col-span-6"
-        overlap
-      />
-      <x-team.portrait 
-        name="Jane Master" 
-        title="Modellbauer" 
-        email="john.doe@example.com" 
-        image="https://picsum.photos/800/1200"
-        class="md:!col-span-6"
-        overlap
-      />
-    </div>
-  </div>
-</div>
-
-<div class="flex flex-col md:grid md:grid-cols-12 gap-20 lg:gap-y-0 mt-45 md:mt-70">
-  <div class="md:col-span-12 lg:col-span-6 bg-red-100 lg:relative">
-    <img src="https://picsum.photos/800/1200" alt="Portrait" class="lg:absolute w-full h-auto lg:h-full object-cover">
-  </div>
-  <div class="md:col-span-12 lg:col-span-6 flex flex-col gap-y-20">
-    <div>
-      <img src="https://picsum.photos/1200/800" alt="Portrait" class="w-full h-auto aspect-[760/500]">
-    </div>
-    <div class="flex flex-col md:grid md:grid-cols-12 gap-y-20 md:gap-y-0 md:gap-x-20">
-      <x-team.portrait 
-        name="John Doe" 
-        title="Projektleiter" 
-        email="john.doe@example.com" 
-        image="https://picsum.photos/800/1200" 
-        class="md:!col-span-6"
-        overlap
-      />
-      <x-team.portrait 
-        name="Jane Master" 
-        title="Modellbauer" 
-        email="john.doe@example.com" 
-        image="https://picsum.photos/800/1200"
-        class="md:!col-span-6"
-        overlap
-      />
-    </div>
-  </div>
-</div> --}}
 @endsection
