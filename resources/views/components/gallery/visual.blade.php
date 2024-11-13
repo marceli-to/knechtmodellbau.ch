2c4c71f7209437c9197d53fe330dc09897090b0d
Marcel Stadelmann <m@marceli.to>
2024-11-13 14:26:25 +0100

    wip

---
+++ resources/views/components/gallery/visual.blade.php
@@ -0,0 +1,23 @@
+@props([
+  'image',
+  'class' => null,
+  'height' => null,
+  'width' => null
+])
+<picture>
+  <source 
+    srcset="/img/{{ $image }}-900w.webp 900w, /img/{{ $image }}-1200w.webp 1200w, /img/{{ $image }}-1600w.webp 1600w, /img/{{ $image }}-2000w.webp 2000w" 
+    type="image/webp">
+  <source 
+    srcset="/img/{{ $image }}-900w.jpg 900w, /img/{{ $image }}-1200w.jpg 1200w, /img/{{ $image }}-1600w.jpg 1600w, /img/{{ $image }}-2000w.jpg 2000w" 
+    type="image/jpeg">
+  <img 
+    src="/img/{{ $image }}-900w.jpg" 
+    alt="" 
+    title="" 
+    height="{{ $height }}" 
+    width="{{ $width }}" 
+    class="w-full h-auto {{ $class }}"
+    loading="lazy">
+</picture>
+
---
+++ resources/views/pages/gallery.blade.php
@@ -0,0 +1,184 @@
+@extends('app')
+@section('site_title', 'Galerie')
+@section('page_title', 'Galerie')
+@section('content')
+
+<div class="flex flex-col gap-y-20">
+  
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="flex flex-col gap-y-20 md:col-span-7 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5 md:col-start-3">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_01"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-7">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_02"
+          :height="405"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5 md:col-start-3">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_03"
+          :height="900"
+          :width="900"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-20 md:col-span-5 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-full">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_04"
+          :height="900"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-full">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_05"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-full">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_06"
+          :height="572"
+          :width="900"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="flex flex-col gap-y-20 md:col-span-5 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_07"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_08"
+          :height="1172"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_09"
+          :height="572"
+          :width="900"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-20 md:col-span-7 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-7">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_10"
+          :height="405"
+          :width="900"
+        />
+      </div>
+     <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_11"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_12"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-7">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_13"
+          :height="405"
+          :width="900"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="flex flex-col gap-y-20 md:col-span-7 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5 md:col-start-3">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_14"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5 md:col-start-3">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_15"
+          :height="572"
+          :width="900"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-20 md:col-span-5 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_16"
+          :height="1172"
+          :width="900"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="md:grid md:grid-cols-12 md:gap-x-20">
+    <div class="flex flex-col gap-y-20 md:col-span-5 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_17"
+          :height="900"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_18"
+          :height="1172"
+          :width="900"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-20 md:col-span-7 md:grid md:grid-cols-subgrid md:gap-20">
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_19"
+          :height="572"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_20"
+          :height="900"
+          :width="900"
+        />
+      </div>
+      <div class="md:col-span-5">
+        <x-gallery.visual 
+          image="knecht_modellbau_galerie_21"
+          :height="572"
+          :width="900"
+        />
+      </div>
+    </div>
+  </div>
+
+</div>
+@endsection
