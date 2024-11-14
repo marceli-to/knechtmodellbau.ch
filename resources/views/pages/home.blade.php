5013f0cc0ecb220b686e2fcb66e46069d8099d16
Marcel Stadelmann <m@marceli.to>
2024-11-14 18:04:26 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section('content')
 <div class="md:grid md:grid-cols-12 md:gap-x-20">
-  <div class="bg-orange-400 mt-140 lg:mt-170 md:col-span-9 lg:col-span-6 md:col-start-3 lg:col-start-2 lg:min-h-[calc(100dvh_-_260px)] lg:flex lg:flex-col lg:justify-between">
+  <div class="mt-140 lg:mt-170 md:col-span-9 lg:col-span-6 md:col-start-3 lg:col-start-2 lg:min-h-[calc(100dvh_-_260px)] lg:flex lg:flex-col lg:justify-between">
     <div>
       <div class="mb-50 md:mb-40 lg:mb-30 prose-width hyphens-auto lg:hyphens-none">
         <p>Seit 1970 bieten wir ein umfassendes Spektrum im Modellbau und verbinden dabei handwerkliches Können mit Leidenschaft und Effizienz. Ob Architektur-, Design-, Prototypenmodelle oder hochwertige Signaletik - wir sind Ihr zuverlässiger Partner für massgeschneiderte Komplettlösungen. Von der optimalen Darstellung über die richtige Materialwahl bis hin zum effizienten Herstellungsprozess stehen wir Ihnen zur Seite.</p>
@@ -23,7 +23,7 @@
     <x-misc.address />
   </div>
   <div 
-    class="hidden lg:relative lg:z-40 lg:block lg:col-span-5 md:col-start-8 lg:max-h-[calc(100dvh_-_260px)]"
+    class="hidden lg:relative lg:z-40 lg:block lg:col-span-5 md:col-start-8"
     :class="{ '!z-10': show_menu }">
     <x-swiper.wrapper>
       <x-swiper.slide image="knecht_modellbau_home_01" alt="" />
