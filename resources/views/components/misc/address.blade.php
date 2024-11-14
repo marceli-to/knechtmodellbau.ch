d30b64421fd98691552a829963d6c81aa0d650df
Marcel Stadelmann <m@marceli.to>
2024-11-14 18:56:12 +0100

    wip

--- resources/views/components/misc/address.blade.php
+++ resources/views/components/misc/address.blade.php
@@ -1,13 +1,13 @@
 <address {{ $attributes->merge(['class' => 'font-semi not-italic']) }}>
   <div class="md:hidden">
     Knecht Modellbau AG<br>
-    Jurastrasse 58<br>
-    5430 Wettingen<br>
+    <a href="https://maps.app.goo.gl/DQBwFr8KMLQygmzm9" target="_blank" class="md:hover:underline md:underline-offset-2 md:decoration-2">Jurastrasse 58<br>
+    5430 Wettingen</a><br>
     <a href="mailto:info@knechtmodellbau.ch" class="hover:underline underline-offset-2 decoration-2">info@knechtmodellbau.ch</a><br>
     <a href="tel:+41 56 426 09 31" class="hover:underline underline-offset-2 decoration-2">+41 56 426 09 31</a>
   </div>
   <div class="hidden md:block">
-    Knecht Modellbau AG, Jurastrasse 58, 5430&nbsp;Wettingen<br>
+    Knecht Modellbau AG, <a href="https://maps.app.goo.gl/DQBwFr8KMLQygmzm9" target="_blank" class="md:hover:underline md:underline-offset-2 md:decoration-2">Jurastrasse 58, 5430&nbsp;Wettingen</a><br>
     <a href="mailto:info@knechtmodellbau.ch" class="md:hover:underline md:underline-offset-2 md:decoration-2">info@knechtmodellbau.ch</a>, 
     <a href="tel:+41 56 426 09 31" class="md:hover:underline md:underline-offset-2 md:decoration-2">+41 56 426 09 31</a>
   </div>
