51964c6fe13f6ae426ee4801a1405dc9d35a52a6
Marcel Stadelmann <m@marceli.to>
2024-11-14 18:30:48 +0100

    wip

--- resources/views/components/menu/main.blade.php
+++ resources/views/components/menu/main.blade.php
@@ -5,14 +5,21 @@
   <nav 
     @class([
       'w-full px-30 md:px-35 lg:px-30',
-      'pt-140 lg:pt-200' => Request::routeIs('page.home'),
-      'pt-110 md:pt-143 lg:pt-163' => !Request::routeIs('page.home'),
+      'pt-173 lg:pt-200' => Request::routeIs('page.home'),
+      'pt-113 md:pt-143 lg:pt-163' => !Request::routeIs('page.home'),
     ])>
-    <ul class="flex flex-col gap-y-16 lg:gap-y-32 leading-none">
+    <ul class="flex flex-col gap-y-24 lg:gap-y-32 leading-none">
       <x-menu.main-item href="page.portrait" title="Portrait" />
       <x-menu.main-item href="page.gallery" title="Galerie" />
       <x-menu.main-item href="page.competences.spectrum" title="Kompetenzen" />
       <x-menu.main-item href="page.contact" title="Kontakt" />
+      <a 
+        href="https://www.instagram.com/knechtmodellbau.ch/"
+        target="_blank"
+        title="Instagram"
+        rel="noopener noreferrer">
+        <x-icons.instagram class="size-20 lg:size-26 mt-6 lg:mt-8" />
+      </a>
     </ul> 
   </nav>
 </div>
