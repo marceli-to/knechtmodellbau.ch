b1d3bf17de0d6564bffbdab70de1808d3be79896
Marcel Stadelmann <m@marceli.to>
2024-11-14 17:58:42 +0100

    wip

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -2,7 +2,7 @@
   role="main" 
   @class([
     'w-full flex-grow relative',
-    'pt-30 pb-30 md:pt-35 md:pb-35 lg:pt-30 lg:pb-0' => Request::routeIs('page.home'),
+    'pt-30 pb-30 md:pt-35 md:pb-35 lg:pt-30 lg:pb-30' => Request::routeIs('page.home'),
     'pt-110 md:pt-140 lg:pt-200 pb-30 md:pb-35 lg:pb-30' => !Request::routeIs('page.home'),
   ])>
   {{ $slot }}
